72bfa502e670d3613b53f5e6e8f4e77470931a33
Francesco Michelini <hello@francescomichelini.com>
2024-08-23 19:51:48 +0200

    Update Rapier's debug mesh

--- src/assets/js/physics/Debug.js
+++ src/assets/js/physics/Debug.js
@@ -20,8 +20,19 @@ export class Debug {
     if (this.enabled) {
       const { vertices, colors } = physics.world.debugRender()
 
-      this.#mesh.geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3))
-      this.#mesh.geometry.setAttribute('color', new Float32BufferAttribute(colors, 4))
+      if (this.#mesh.geometry.hasAttribute('position')) {
+        const { count } = this.#mesh.geometry.attributes.position
+
+        let i
+        for (i = 0; i < count; i++) {
+          this.#mesh.geometry.attributes.position.setXYZ(i, vertices[i * 3 + 0], vertices[i * 3 + 1], vertices[i * 3 + 2])
+        }
+
+        this.#mesh.geometry.attributes.position.needsUpdate = true
+      } else {
+        this.#mesh.geometry.setAttribute('position', new Float32BufferAttribute(vertices, 3))
+        this.#mesh.geometry.setAttribute('color', new Float32BufferAttribute(colors, 4))
+      }
 
       this.#mesh.visible = true
     } else {
